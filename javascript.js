9636dfc4aab38b634d4f3c34f91c84a5bd7f60c7
Ashu Bro <mr.ashu1810@gmail.com>
2023-03-04 21:29:22 +0530

    clock and date added to html with working perfectly

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    
+    if (hrs>12){
+        hrs="0"+String(hrs-12)
+    }
+    if (min<10){
+        min='0'+String(min)
+    }
+
+    document.getElementById('hrs').innerHTML = hrs;
+    document.getElementById('mins').innerHTML = min;
+    
+}
+setInterval(displayTime, 10);
+
+function date(){
+    var date=new Date();
+    var dd=date.getDate();
+    var mm=date.getMonth();
+    var yy=date.getFullYear();
+
+    if (dd<10) {
+        dd='0'+String(dd)
+    }
+    if (mm<10){
+        mm='0'+String(mm)
+    }
+
+    var f_date=dd+'/'+mm+'/'+yy
+    document.getElementById('date').innerHTML=f_date;
+}
+setInterval(date,10);
